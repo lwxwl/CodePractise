c6624930a6879af170426ec742966b9501f79961
lwxwl <673834671@qq.com>
2017-04-07 17:15:42 +0800

    ActivityTest

--- Android/Chapter2/AndroidTest/app/src/main/java/com/example/lwxwl/androidtest/FirstActivity.java
+++ Android/Chapter2/AndroidTest/app/src/main/java/com/example/lwxwl/androidtest/FirstActivity.java
@@ -1,7 +1,9 @@
 package com.example.lwxwl.androidtest;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -18,8 +20,38 @@ public class FirstActivity extends AppCompatActivity {
         button1.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                Toast.makeText(FirstActivity.this, "You clicked Button 1", Toast.LENGTH_SHORT).show();
+                //Toast.makeText(FirstActivity.this, "You clicked Button 1", Toast.LENGTH_SHORT).show();
                 // finish();  销毁活动
+
+                // 显式Intent
+                //Intent intent = new Intent(FirstActivity.this, SecondActivity.class);
+                //startActivity(intent);
+
+                // 隐式Intent
+                // Intent intent = new Intent("com.example.lwxwl.androidtest.ACTION_START");
+                // intent.addCategory("com.example.lwxwl.androidtest.MY_CATEGORY");
+                // startActivity(intent);
+
+                // 打开浏览器，但并无加载并显示网页的功能
+                // Intent intent = new Intent(Intent.ACTION_VIEW);
+                // intent.setData(Uri.parse("http://www.baidu.com"));
+                // startActivity(intent);
+
+                // 调用系统拨号界面
+                // Intent intent = new Intent(Intent.ACTION_DIAL);
+                // intent.setData(Uri.parse("tel:10086"));
+                // startActivity(intent);
+
+                // 向SecondActivity传递数据
+                // String data = "Hello SecondActivity";
+                // Intent intent = new Intent(FirstActivity.this, SecondActivity.class);
+                // intent.putExtra("extra_data", data);
+                // startActivity(intent);
+
+                // 获取SecondActivity返回的数据
+                Intent intent = new Intent(FirstActivity.this, SecondActivity.class);
+                startActivityForResult(intent, 1);
+
             }
         });
     }
@@ -42,5 +74,20 @@ public class FirstActivity extends AppCompatActivity {
             default:
         }
         return true;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch(requestCode){
+            case 1:
+                if(resultCode == RESULT_OK){
+                    String returnedData = data.getStringExtra("data_returned");
+                    Log.d("FirstActivity", returnedData);
+                }
+                break;
+            default:
+        }
+
+
     }
 }
---
+++ Android/Chapter2/AndroidTest/app/src/main/java/com/example/lwxwl/androidtest/SecondActivity.java
@@ -0,0 +1,42 @@
+package com.example.lwxwl.androidtest;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_layout);
+
+        // 取出FirstActivity中传递的数据
+        // Intent intent = getIntent();
+        // String data = intent.getStringExtra("extra_data");
+        // Log.d("SecondActivity", data);  // 打印
+
+        // 向FirstActivity返回数据
+        Button button2 = (Button) findViewById(R.id.button_2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.putExtra("data_return", "Hello FirstActivity");
+                setResult(RESULT_OK, intent);
+                finish();
+            }
+        });
+    }
+
+    // 按下back键执行的方法
+    @Override
+    public void onBackPressed() {
+        Intent intent = new Intent();
+        intent.putExtra("data_return", "Hello FirstActivity");
+        setResult(RESULT_OK, intent);
+        finish();
+    }
+}
