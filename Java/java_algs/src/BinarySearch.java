50c388c712d784f926586dc5da79a5cebe2052d1
lwxwl <673834671@qq.com>
2017-04-18 22:29:53 +0800

    java

---
+++ Java/java_algs/src/BinarySearch.java
@@ -0,0 +1,20 @@
+public class BinarySearch {
+
+    public static int rank(int key, int[] a) {
+        return rank(key, a, 0, a.length - 1);
+    }
+
+    public static int rank(int key, int[] a, int low, int high) {
+        if (low > high) {
+            return -1;
+        }
+        int mid = low + (high - low) / 2;
+        if (key < a[mid]) {
+            return  rank(key, a, low, mid - 1);
+        } else if (key > a[mid]) {
+            return rank(key, a, mid + 1, high);
+        } else {
+            return mid;
+        }
+    }
+}
